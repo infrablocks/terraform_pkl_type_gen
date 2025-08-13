ff5b92a6b8a436af07345f5485a1bdb539ee5761
Corin Chaplin <corin@go-atomic.io>
2025-08-13 14:33:57 +0100

    Tests

--- tests/unit/infrablocks/test_map_type.py
+++ tests/unit/infrablocks/test_map_type.py
@@ -12,28 +12,28 @@ class TestTerraformPklTypeMapping:
         assert map_type("bool") == "Boolean"
 
     def test_list_of_dynamic_type_mapping(self):
-        assert map_type("list of dynamic") == "Listing"
+        assert map_type("list of dynamic") == "Listing | List"
 
     def test_list_of_any_type_mapping(self):
-        assert map_type("list(any)") == "Listing"
+        assert map_type("list(any)") == "Listing | List"
 
     def test_list_of_string_type_mapping(self):
-        assert map_type("list of string") == "Listing<String>"
+        assert map_type("list of string") == "Listing<String> | List<String>"
 
     def test_list_of_number_type_mapping(self):
-        assert map_type("list of number") == "Listing<Number>"
+        assert map_type("list of number") == "Listing<Number> | List<Number>"
 
     def test_list_of_bool_type_mapping(self):
-        assert map_type("list of bool") == "Listing<Boolean>"
+        assert map_type("list of bool") == "Listing<Boolean> | List<Boolean>"
 
     def test_map_of_dynamic_type_mapping(self):
-        assert map_type("map of dynamic") == "Mapping<String, Any>"
+        assert map_type("map of dynamic") == "Mapping<String, Any> | Map<String, Any>"
 
     def test_map_of_string_type_mapping(self):
-        assert map_type("map of string") == "Mapping<String, String>"
+        assert map_type("map of string") == "Mapping<String, String> | Map<String, String>"
 
     def test_map_of_number_type_mapping(self):
-        assert map_type("map of number") == "Mapping<String, Number>"
+        assert map_type("map of number") == "Mapping<String, Number> | Map<String, Number>"
 
     def test_map_of_bool_type_mapping(self):
-        assert map_type("map of bool") == "Mapping<String, Boolean>"
+        assert map_type("map of bool") == "Mapping<String, Boolean> | Map<String, Boolean>"
